91cad45143af5a20d0a6b4b97b3d8dc7051162c5
starkiller42 <luc_ssoares@outlook.com>
2020-08-02 09:23:34 -0300

    alteracao em categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias'
+    const URL = window.location.hostname.includes('localhost') 
+      ? 'http://localhost:3000/categorias'
+      : 'https://lucas-flix.herokuapp.com/categorias';
     fetch(URL).then(async (respostaDoServidor) => {
       const resposta = await respostaDoServidor.json();
       setCategorias([
